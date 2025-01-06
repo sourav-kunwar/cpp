d9cbe00578619526cc32cbcdb2cef189b1586eb4
=sourav singh kunwar <souravsinghkunwar1@gmail.com>
2025-01-06 12:04:00 +0530

    bubble sort

---
+++ bubble_sort_new.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+void bubble_sort(int arr[], int n)
+{
+    for(int i = n-1;i>=0;i--)
+    {
+        for(int j = 0;j < i;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                int temp = arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]= temp;
+            }
+        }
+    }
+    
+    for(int i =0 ;i<n;i++)
+    {
+        cout<<arr[i]<<",";
+    }
+}
+
+int main()
+{
+    int arr[]={22,34,31,13,1,3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    bubble_sort(arr,n);
+    return 0;
+}
