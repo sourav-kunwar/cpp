41daf50134f2ad7d40d52d57ae041c387b57f969
=sourav singh kunwar <souravsinghkunwar1@gmail.com>
2025-01-06 12:09:54 +0530

    did_swap is added to check if swapping is done or not if done than stop the sorting

--- bubble_sort_new.cpp
+++ bubble_sort_new.cpp
@@ -7,6 +7,7 @@ void bubble_sort(int arr[], int n)
 {
     for(int i = n-1;i>=0;i--)
     {
+        int did_swap = 0;
         for(int j = 0;j < i;j++)
         {
             if(arr[j]>arr[j+1])
@@ -14,7 +15,12 @@ void bubble_sort(int arr[], int n)
                 int temp = arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]= temp;
+                did_swap = 1;
             }
+        }
+        if(did_swap==0)
+        {
+            break;
         }
     }
     
