4866094f698ac9162a78019d0f152cfda8079f2e
sourav-kunwar <souravsinghkunwar1@gmail.com>
2025-02-21 16:54:52 +0530

    new commit

--- pointers.cpp
+++ pointers.cpp
@@ -2,25 +2,66 @@
 
 using namespace std;
 
+void passvalue(int a){
+    a = 10;
+}
+
+void passreference(int *a)
+{
+    *a = 30;
+}
+void passalias(int &b)
+{
+    b=50;
+}
 int main(){
+    // int a = 10;
+    // // cout<<&a<<endl;
+
+    //  int *ptr = &a;
+    // // cout<<ptr<<endl;
+
+    // //pointer to pointer
+
+    // int** parptr = &ptr;
+    // cout<<&ptr<<endl;
+    // cout<<parptr;
+
+    // //dereferencing
+
+    // cout<<"\nDereferencing pointer:"<<*(&a)<<endl;
+    // cout<<"using pointer:"<<*(ptr);
+    // cout<<"\nusing parentpointer:"<<**parptr;
+
+    //passby value:
+
     int a = 10;
-    // cout<<&a<<endl;
+    passvalue(a);
+    cout<<"pass by value:"<<a<<endl;
 
-     int *ptr = &a;
-    // cout<<ptr<<endl;
+    // pass by reference
 
-    //pointer to pointer
+    passreference(&a);
+    cout<<"pass by reference:"<<a<<endl;
 
-    int** parptr = &ptr;
-    cout<<&ptr<<endl;
-    cout<<parptr;
+    //pass by reference using alias;
 
-    //dereferencing
+    a = 23;
+    passalias(a);
+    cout<<"pass by reference using alias:"<<a<<endl;
 
-    cout<<"\nDereferencing pointer:"<<*(&a)<<endl;
-    cout<<"using pointer:"<<*(ptr);
-    cout<<"\nusing parentpointer:"<<**parptr;
+    //array pointer
 
-    
+    int arr[] = {1,2,3,4,5,6};
+    //array pointer is constant pointer as its value can't be changed after initialization
+
+    //pointer increment and decrement
+    int m =1;
+    int* ptr = &m;
+    cout<<ptr<<endl;
+    *(ptr++);
+    cout<<ptr;
+    cout<<m;
+
     return 0;
 }
